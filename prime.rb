18391d62d6a554345dbfb86c32e9a60c20eedd14
jameslee1187@gmail.com <jameslee1187@gmail.com>
2018-09-09 15:47:57 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(num)
+  if num < 2
+    return false
+  end
+  (2...num).each do |ele|
+    if num % ele == 0
+      return false
+    end
+  end
+  return true
+end
+    
+    
+    # Add  code here!
