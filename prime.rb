ab580f28cc5b1224c986c82d924271e21c409770
gianna.jolibeth15 <gianna.jolibeth15@gmail.com>
2018-09-30 22:57:43 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
 # Add  code here!
+def prime?(int)
+if int == 0 || int <= 1 
+return false 
+end 
+if int ==2 
+return true 
+end 
+
+counter = 2
+while counter < int 
+if int% counter ==0
+return false 
+end 
+counter +=1
+ end
+ return true  
+ end 
